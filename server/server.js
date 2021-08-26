459feba7df97aadd268ed04d6af075bba3816a79
Jimmy Tran <jimmytran@Jimmys-MacBook-Pro.local>
2021-08-26 16:23:20 -0400

    finished adding auth service

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
+// configure the development .env file
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
 // required modules
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const morgan = require('morgan')
 const mongoose = require('mongoose')
 const mainRouter = require('./src/routes/main.route')
 const authRouter = require('./src/routes/auth.route')
 const userRouter = require('./src/routes/user.route')
-const morgan = require('morgan')
 
-// configure the development .env file
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 // connect to the database
 mongoose.connect(process.env.DB_URL, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => err ? console.log('error connecting to the db') : console.log('connected to db successfully')); // connect to the database
 
--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,106 +1,40 @@
-
-const bcrypt = require('bcrypt');
-const JwtUtil = require('../utils/jwt.util');
-const User = require('../model/userSchema');
+'use strict'
+const AuthService = require('../services/auth.service')
 
 const signIn = (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-
-    // query to look for the user by username
-    User.findOne({ username: username }, (err, user) => {
-        if (err) {
-            return res.json({
-                success: false,
-                message: err
-            })
-        }
-        if (!user) {
-            return res.json({
-                success: false,
-                message: 'User does not exist!'
-            })
-        }
-        // create a user object to pass into generateToken and generaterefreshToken
-        let user_object = { username:user.username, name:user.name }
-        // compare the password
-        bcrypt.compare(password, user.password, (err, result) => {
-            if (err) {
-                return res.json({
-                    message: err,
-                    success: false
-                })
-            }
-            if (result === true) {
-                // create the data object 
-                let data = {
-                    user: user,
-                    accessToken: JwtUtil.generateToken(user_object),
-                    refreshToken: JwtUtil.generateRefreshToken(user_object)
-                }
-                return res.json({
-                    message: 'User successfully logged in',
-                    success: true,
-                    data: data
-              })
-            } else {
-                return res.json({
-                    message: 'Password is wrong!',
-                    success: false
-                })
-            }
-        });
-
+    AuthService.signUserIn(req.body.username, req.body.password, (err, result, data) =>{
+        return res.json({
+            message: err ? err : result,
+            success: err ? false : true,
+            data: data
+        })
     })
+}
+
+const refreshToken = (req, res) => {
+    AuthService.refreshToken(req.body.refresh, (err, result, status) => {
+        if (status != 200) {
+            return res.status(status).json({
+                message: err ? err : result
+            })
+        }
+        else {
+            return res.status(status).json({
+                result
+            })
+        }
+    })   
 }
 
 const verify = (req,res) => {
     // get the token from the headers
     let token = req.headers.authorization.split(' ')[1];
-    // verify the token
-    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,result) => {
-        if (err) {
-            return res.json({
-                message:err,
-                success:false
-            })
-        }
-        else {
-            return res.json({
-                message:result,
-                success:true
-            })
-        }
-    })
-}
-
-const refreshToken = (req, res) => {
-    const refresh = req.body.refresh
-    if (!refresh) res.status(403).json({ message: 'No available' });
-
-    // check if the refresh token exists 
-    if (!auth.refreshTheToken(refresh)) {
-        res.status(403).json({ message: 'invalid refresh token' });
-    }
-    else {
-        // verify the refresh if it does exist
-        jwt.verify(refresh, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-            // if err, then the token sent is not valid
-            if (err) {
-                res.sendStatus(403);
-                res.end();
-            }
-            // generate a new token if valid
-            else {
-                const newAccessToken = auth.generateToken({ name: user.name })
-                res.status(200).json({
-                    accessToken: newAccessToken,
-                    refreshToken: refresh
-                })
-
-            }
+    AuthService.verify(token, (err,result) => {
+        return res.json({
+            message: err ? err : result,
+            success: err ? false: true
         })
-    }
+    })
 }
 
 module.exports = {

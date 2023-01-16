2c240abb1d631a3b6914f342796057f05e34d1c0
jimmytran16 <jimmytran1620@gmail.com>
2023-01-16 18:22:28 -0500

    update for glitch

--- twitter-client/src/Configs.js
+++ twitter-client/src/Configs.js
@@ -1,5 +1,6 @@
 const CONFIGS = {
-    SERVER_URL:'http://localhost:3001'
+    SERVER_URL: 'https://fancy-glitter-deerstalker.glitch.me'
+    // SERVER_URL:'http://localhost:3001'
     // SERVER_URL:'https://twitter-cl0ne-api-jwt.herokuapp.com'
 }
 
